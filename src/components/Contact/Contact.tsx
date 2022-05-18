56ab7dc8da6bf812d168e54e6093c6654f7a2c3f
Tom Hibbers <38688272+tomhibbers@users.noreply.github.com>
2022-05-18 02:48:17 +0200

    Update contact buttons. Move contact form into contact page to share recaptcha

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,64 +1,136 @@
 import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
-import ReCAPTCHA from 'react-google-recaptcha';
-import { useState } from 'react'
+import phoneIcon from "../../assets/phone-icon.svg";
+import ReCAPTCHA from "react-google-recaptcha";
 // import React from 'react';
 // import { ThemeContext } from '../../context/themeContext';
 // import { ThemeContextType } from '../../@types/theme';
+import { useState } from "react";
 
-export function Contact(){
-  const [email, setEmail] = useState('#');
-  const [phone, setPhone] = useState('');
-  const [isHuman, setHuman] = useState(false);
-  const REACT_APP_CAPTCHA_SITE_KEY: string = process.env.REACT_APP_CAPTCHA_SITE_KEY!;
-  // const { theme } = React.useContext(ThemeContext) as ThemeContextType;
-  // const recaptchaRef = React.createRef<ReCAPTCHA>();
+// import { ContainerSucces } from './styles'
+// import { Form } from "../Form/Form";
+// import { useEffect } from "react";
+// import validator from "validator";
+// import { useForm, ValidationError } from "@formspree/react";
+// import { toast, ToastContainer } from "react-toastify";
 
-  // React.useEffect(() => {
-  //   if(recaptchaRef.current){
-  //     recaptchaRef.current.reset();
+export function Contact() {
+  // const [state, handleSubmit] = useForm("myyozglw");
+  // const [validEmail, setValidEmail] = useState(false);
+  // const [message, setMessage] = useState("");
+  const [emailText, setEmailText] = useState("###@###.com");
+  const [phoneText, setPhoneText] = useState("(+27) ##-###-####");
+  const [isHuman, setIsHuman] = useState(false);
+  const REACT_APP_CAPTCHA_SITE_KEY: string =
+    process.env.REACT_APP_CAPTCHA_SITE_KEY!;
+
+  // function verifyEmail(email: string) {
+  //   if (validator.isEmail(email)) {
+  //     setValidEmail(true);
+  //   } else {
+  //     setValidEmail(false);
   //   }
-  // }, [theme]);
-  
-  function enableContactDetails(this: any) {
-    setEmail("mailto:tomhibbers@gmail.com");
-    setPhone("tel:+27822987183");
-    setHuman(true);
-  };
+  // }
 
-  return(
+  // useEffect(() => {
+  //   if (state.succeeded) {
+  //     toast.success("Email successfully sent!", {
+  //       position: toast.POSITION.BOTTOM_LEFT,
+  //       pauseOnFocusLoss: false,
+  //       closeOnClick: true,
+  //       hideProgressBar: false,
+  //       toastId: "succeeded",
+  //     });
+  //   }
+  // });
+  // if (state.succeeded) {
+  //   return (
+  //     <ContainerSucces>
+  //       <h3>Thank you for contacting me!</h3>
+  //       <button
+  //         onClick={() => {
+  //           window.scrollTo({ top: 0, behavior: "smooth" });
+  //         }}
+  //       >
+  //         Back to the top
+  //       </button>
+  //       <ToastContainer />
+  //     </ContainerSucces>
+  //   );
+  // }
+  function enableContactDetails(this: any) {
+    setEmailText("tomhibbers@gmail.com");
+    setPhoneText("(+27) 82-298-7183");
+    setIsHuman(true);
+  }
+  function onClickEmail() {
+    window.open("mailto:tomhibbers@gmail.com", "_blank");
+  }
+  function onClickPhone() {
+    window.open("tel:+27822987183", "_blank");
+  }
+
+  return (
     <Container id="contact">
       <header>
         <h2>Contact Me</h2>
-        <p>If you've seen my potential or want to talk to me, don't hesitate to send me a message.</p>
+        <p>
+          If you've seen my potential or want to talk to me, don't hesitate to
+          send me a message.
+        </p>
       </header>
       <div className="contacts">
-        <a href={email} target="_blank" rel="noreferrer">
-          <button
-            type="button"
-            disabled={!isHuman}
-          >
-            <img src={emailIcon} alt="Email" />
-          </button>
-        </a>
-        <a href={phone} target="_blank" rel="noreferrer">
-          <button
-            type="button"
-            disabled={!isHuman}
-          >
-            <img src={phoneIcon} alt="Phone" />
-          </button>
-        </a>
-        <ReCAPTCHA 
+        <button type="button" disabled={!isHuman} onClick={onClickEmail}>
+          <img src={emailIcon} alt="Email" />
+          <p>{emailText}</p>
+        </button>
+        <button type="button" disabled={!isHuman} onClick={onClickPhone}>
+          <img src={phoneIcon} alt="Phone" />
+          <p>{phoneText}</p>
+        </button>
+      </div>
+      <form>
+      {/* <form onSubmit={handleSubmit}> */}
+        {/* <h2>Get in touch using the form below</h2>
+        <input
+          placeholder="Email"
+          id="email"
+          type="email"
+          name="email"
+          onChange={(e) => {
+            verifyEmail(e.target.value)
+          }}
+          required
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <textarea
+          required
+          placeholder="Leave your message"
+          id="message"
+          name="message"
+          onChange={(e) => {
+            setMessage(e.target.value)
+          }}
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        /> */}
+        <div className="reCaptcha-Wrapper">
+          <ReCAPTCHA
             sitekey={REACT_APP_CAPTCHA_SITE_KEY}
             onChange={enableContactDetails}
-            theme='dark'
-            // size="compact"
-        ></ReCAPTCHA>
-      </div>
-        
-      {/* <Form></Form> */}
+            theme="dark"
+          ></ReCAPTCHA>
+        </div>
+        {/* <button
+          type="submit"
+          disabled={state.submitting || !validEmail || !message || !isHuman}
+        >
+          Send
+        </button> */}
+      </form>
     </Container>
-  )
+  );
 }
